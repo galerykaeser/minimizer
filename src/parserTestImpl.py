7bfe9560a2d50bb12970564ceb3039aa47a35d20
Lucas <lucas.galerykaeser@gmail.com>
2020-10-24 17:08:53 +0200

    Updated parser interface implementation example

--- src/parserTestImpl.py
+++ src/parserTestImpl.py
@@ -2,7 +2,7 @@ from parserbase import ParserBase
 
 
 class Parser(ParserBase):
-    def parse_output_string(self, output_string) -> bool:
-        cond1 = "caught signal 11 -- exiting" in output_string
-        cond2 = "caught signal 6 -- exiting" in output_string
+    def parse(self, output):
+        cond1 = "caught signal 11 -- exiting" in output
+        cond2 = "caught signal 6 -- exiting" in output
         return cond1 or cond2
